5a7fb75aea76a82996d08a3806b4742050701b73
Cesar Kohl <cesarkohl@gmail.com>
2020-07-10 17:27:46 -0300

    Add component section__parallax_scroll.

---
+++ section__parallax_scroll/src/app.js
@@ -0,0 +1,18 @@
+function asOneParallax() {
+  let bg = document.querySelector('.bg');
+  let moon = document.querySelector('.moon');
+  let mountain = document.querySelector('.mountain');
+  let road = document.querySelector('.road');
+  let title = document.querySelector('.as-one-parallax__title');
+
+  window.addEventListener('scroll', (e) => {
+    let value = window.scrollY;
+
+    bg.style.top = -value * 0.01 + 'px';
+    moon.style.left = -value * 0.15 + 'px';
+    mountain.style.top = value * 0.15 + 'px';
+    road.style.top = value * 0.1 + 'px';
+    // title.style.top = value * 0.2 + 'px';
+  })
+}
+asOneParallax();
