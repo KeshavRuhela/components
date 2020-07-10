16a4f0963f83a46aa0dde59bafa07da8fbf19eee
Cesar Kohl <cesarkohl@gmail.com>
2020-07-10 10:30:50 -0300

    Add scroll-progress-bar.

---
+++ scroll-progress-bar/app.js
@@ -0,0 +1,12 @@
+function asOneProgressBar() {
+
+  let progressBar = document.querySelector('.as-one__progress-bar');
+
+  window.onscroll = () => {
+    let viewportHeight = document.body.scrollHeight - window.innerHeight;
+    let progressHeight = (window.pageYOffset / viewportHeight) * 100;
+    progressBar.style.height = progressHeight + "%";
+  }
+
+}
+asOneProgressBar();
