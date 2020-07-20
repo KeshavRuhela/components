8840a4ac034fac52cffdbf197faf3827c4b8285d
Cesar Kohl <cesarkohl@gmail.com>
2020-07-20 19:04:13 -0300

    Updated as requested.

--- scroll-skewed-background__transform/src/app.js
+++ scroll-skewed-background__transform/src/app.js
@@ -1,5 +1,6 @@
 let skewed = document.querySelector('.as-one-scroll__skewed');
+
 window.addEventListener('scroll', function() {
-  let value = -10 + window.scrollY/60;
-  skewed.style.transform = "skewY("+ value +"deg)"
+  let value = -10 + window.scrollY / 60;
+  skewed.style.transform = "skewY(" + value + "deg)";
 });
