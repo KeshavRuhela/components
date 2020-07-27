bbad9ed14c693599bfae0b03b714660938146f0e
Cesar Kohl <cesarkohl@gmail.com>
2020-07-27 13:50:53 -0300

    Update some app.js in /applied.

--- applied/cursor-mousemove-circle-hover-scale/src/app.js
+++ applied/cursor-mousemove-circle-hover-scale/src/app.js
@@ -1,7 +1,15 @@
-let cursor = document.querySelector('.ao-cursor');
-document.addEventListener('mousemove', function(e) {
-  let x = e.clientX;
-  let y = e.clientY;
-  cursor.style.left = x + "px";
-  cursor.style.top = y + "px";
-});
+function AoCursorMousemoveCircleHoverScale()
+{
+  let cursor = document.querySelector('.ao-cursor');
+
+  // Logic
+  document.addEventListener('mousemove', function(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+  });
+}
+
+export default AoCursorMousemoveCircleHoverScale;
