16b876111ca82ef15d457305635baa2cec721353
Julie Kohl <julianacochenski@Julianas-MacBook-Air.local>
2020-07-30 15:38:03 -0300

    Add /navigation-full-screen-overlay.

---
+++ applied/navigation-full-screen-overlay/src/app.js
@@ -0,0 +1,6 @@
+function menuToggel() {
+  let nav = document.querySelector('.ao-fullpage');
+  let toggle = document.querySelector('.toggle');
+  nav.classList.toggle("active")
+  toggle.classList.toggle("active")
+};
