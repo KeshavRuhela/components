2379ed6b1888945922706d661e9b74aaccedae71
Cesar Kohl <cesarkohl@gmail.com>
2020-08-05 13:09:16 -0300

    Update JS.

--- applied/navigation-full-screen-overlay/src/app.js
+++ applied/navigation-full-screen-overlay/src/app.js
@@ -1,13 +1,14 @@
+let classNav = 'ao-fullpage';
 let classFullpageToggle = "ao-menuicon";
+
+let nav = document.querySelector(`.${classNav}`);
 let fullpageToggle = document.querySelector(`.${classFullpageToggle}`);
 
-function menuToggle() {
-  let nav = document.querySelector('.ao-fullpage');
-  let toggle = document.querySelector('.toggle');
+function clickFullpageToggle() {
   nav.classList.toggle("active");
-  toggle.classList.toggle("active");
+  fullpageToggle.classList.toggle("active");
 };
 
 fullpageToggle.addEventListener('click', (e) => {
-  menuToggle();
+  clickFullpageToggle();
 })
